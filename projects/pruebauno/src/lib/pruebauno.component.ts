a1c09dde03ac3b12fd8ddc4b1826f8685652ad74
Ramos Ivan Lautaro <ivanramos_8@hotmail.com>
2023-03-14 10:28:10 -0300

    primera actualizacion

--- projects/pruebauno/src/lib/pruebauno.component.ts
+++ projects/pruebauno/src/lib/pruebauno.component.ts
@@ -3,11 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'lib-pruebauno',
   template: `
+  <input type="color">
     <button>Acceder</button>
     <button>Color</button>
     <button type="button">Acceder</button>
+    <button type="button">Acceder</button>
+    <button type="button">Acceder</button>
     <input type="color">
-  `,
+    `,
   styles: [
   ]
 })
